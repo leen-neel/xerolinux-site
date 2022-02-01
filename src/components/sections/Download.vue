e24e290283974bf5664d952137612db56f9023b3
leen-neel <and24903@gmail.com>
2022-02-01 13:20:37 +0530

    download section now functional

--- src/components/sections/Download.vue
+++ src/components/sections/Download.vue
@@ -2,14 +2,11 @@
   <section id="download" class="q-pa-md">
     <h2 class="text-center">Download</h2>
 
-    <h4 class="text-bold">XeroLinux KDE</h4>
+    <h4 class="text-bold">XeroLinux {{ editions[selectedEditionID].name }}</h4>
     <div :class="{ row: $q.screen.gt.sm, column: $q.screen.lt.md }">
       <div class="col-6">
         <p>
-          This is our "Flagship" release, it uses KDE as its DE and offers a
-          wide variety of packages to choose from during install, support for a
-          wide host of Hardware, not to mention the level of customisation that
-          KDE brings with it.
+          {{ editions[selectedEditionID].desc }}
         </p>
 
         <div class="row q-gutter-sm">
@@ -19,7 +16,7 @@
             class="q-ma-sm"
             icon="note"
             type="a"
-            href="https://forum.xerolinux.xyz/thread-4.html"
+            :href="editions[selectedEditionID].releaseNotes"
             target="_blank"
           />
           <q-btn
@@ -28,7 +25,7 @@
             color="accent"
             class="q-ma-sm"
             type="a"
-            href="https://sourceforge.net/projects/xerolinux/files/Releases/Main/xerolinux-main-x86_64.iso/download"
+            :href="editions[selectedEditionID].downloadLink"
             target="_blank"
           />
           <q-btn
@@ -44,9 +41,24 @@
 
         <div class="text-h4 q-mt-md">Other Edtitions</div>
         <div class="row q-gutter-sm q-mt-sm">
-          <q-btn label="KDE" color="accent" />
-          <q-btn label="GNOME" color="accent" />
-          <q-btn label="XFCE" color="accent" />
+          <q-btn
+            v-if="selectedEditionID !== 'kde'"
+            label="KDE"
+            color="accent"
+            @click="selectedEditionID = 'kde'"
+          />
+          <q-btn
+            v-if="selectedEditionID !== 'gnome'"
+            label="GNOME"
+            color="accent"
+            @click="selectedEditionID = 'gnome'"
+          />
+          <q-btn
+            v-if="selectedEditionID !== 'xfce'"
+            label="XFCE"
+            color="accent"
+            @click="selectedEditionID = 'xfce'"
+          />
         </div>
       </div>
 
@@ -68,6 +80,47 @@
     </div>
   </section>
 </template>
+
+<script>
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const selectedEditionID = ref("kde");
+
+    const editions = ref({
+      kde: {
+        name: "KDE",
+        desc: 'This is our "Flagship" release, it uses KDE as its DE and offers a wide variety of packages to choose from during install, support for a wide host of Hardware, not to mention the level of customisation that KDE brings with it.',
+        downloadLink:
+          "https://sourceforge.net/projects/xerolinux/files/Releases/Main/xerolinux-main-x86_64.iso/download",
+        releaseNotes: "https://forum.xerolinux.xyz/thread-4.html",
+      },
+
+      xfce: {
+        name: "XFCE",
+        desc: "While this one was created for all of you out there that don't have powerful enough hardware to run our \"Flagship\" release that uses tons of effects and other tools that use a lot of system resources. It uses XFCE as its DE, as it's one of the lightest out there, that can still be made to look great without impacting performance. Don't worry though, it still retains most of the main features but with a bit less options.",
+        downloadLink:
+          "https://sourceforge.net/projects/xerolinux/files/Releases/XFCE/xerolinux-xfce-x86_64.iso/download",
+        releaseNotes: "https://forum.xerolinux.xyz/thread-14.html",
+      },
+
+      gnome: {
+        name: "GNOME",
+        desc: "This was requested hard on both Discord and Telegram. I succumb to the pressure and released it. It fully supports Wayland on nVidia cards, that's the biggest part. Otherwise it's just basic, I opted not to use too many extensions as GNOME is notorious for having them break with every major upgrade. So in essence this edition only exists because of it being requested so hard. A new version will only be released on major events like, New Year's, Valentine's, Easter, Thanksgiving and Christmas. Not as frequently as the others. This is the last edition to be added to the family. I hope you like it.",
+        downloadLink:
+          "https://sourceforge.net/projects/xerolinux/files/Releases/GNOME/xerolinux-gnome-x86_64.iso/download",
+        releaseNotes: "https://forum.xerolinux.xyz/thread-66.html",
+      },
+    });
+
+    return {
+      selectedEditionID,
+      editions,
+    };
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 .header {
