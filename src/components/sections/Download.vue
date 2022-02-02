ba63f122091be82bd885f91b164f6f335c10cf2f
leen-neel <and24903@gmail.com>
2022-02-02 17:36:03 +0530

    changes to download section

--- src/components/sections/Download.vue
+++ src/components/sections/Download.vue
@@ -1,101 +1,58 @@
 <template>
-  <section id="download" class="q-pa-md edition-bg radius-16">
-    <div class="flex flex-center column">
-      <div
-        class="text-center"
-        :class="{
-          'text-h3': $q.screen.gt.sm,
-          'text-h6 text-bold': $q.screen.lt.md,
-        }"
-      >
-        .:: Choose Your Edition ::.
-      </div>
-
-      <div class="row q-gutter-lg q-mt-md q-mb-lg">
-        <q-btn
-          v-if="selectedEditionID !== 'kde'"
-          label="KDE"
-          color="accent"
-          @click="selectedEditionID = 'kde'"
-          size="18px"
-        />
-        <q-btn
-          v-if="selectedEditionID !== 'gnome'"
-          label="GNOME"
-          color="accent"
-          @click="selectedEditionID = 'gnome'"
-          size="18px"
-        />
-        <q-btn
-          v-if="selectedEditionID !== 'xfce'"
-          label="XFCE"
-          color="accent"
-          @click="selectedEditionID = 'xfce'"
-          size="18px"
-        />
-      </div>
-    </div>
-
-    <div class="text-h4 q-mb-sm text-bold">
-      XeroLinux {{ editions[selectedEditionID].name }}
-    </div>
-
-    <div :class="{ row: $q.screen.gt.sm, column: $q.screen.lt.md }">
-      <div class="col-6 q-mt-md">
-        <p
+  <div class="flex flex-center">
+    <section
+      id="download"
+      class="q-pa-md edition-bg radius-16"
+      style="width: 97.5vw"
+    >
+      <div class="flex flex-center column">
+        <div
+          class="text-center"
           :class="{
-            'width-control': $q.screen.gt.sm,
+            'text-h3': $q.screen.gt.sm,
+            'text-h6 text-bold': $q.screen.lt.md,
           }"
-          v-html="editions[selectedEditionID].desc"
-        ></p>
-
-        <!-- These buttons appear under the description on desktop -->
-        <div v-if="$q.screen.gt.sm" class="row q-mt-md q-gutter-sm">
+        >
+          .:: Choose Your Edition ::.
+        </div>
+        <div class="row q-gutter-lg q-mt-md q-mb-lg">
           <q-btn
-            label="Release notes"
+            v-if="selectedEditionID !== 'kde'"
+            label="KDE"
             color="accent"
-            class="q-ma-sm"
-            icon="note"
-            type="a"
-            :href="editions[selectedEditionID].releaseNotes"
-            target="_blank"
+            @click="selectedEditionID = 'kde'"
+            size="18px"
           />
           <q-btn
-            label="Download now"
-            icon="download"
+            v-if="selectedEditionID !== 'gnome'"
+            label="GNOME"
             color="accent"
-            class="q-ma-sm"
-            type="a"
-            :href="editions[selectedEditionID].downloadLink"
-            target="_blank"
+            @click="selectedEditionID = 'gnome'"
+            size="18px"
           />
           <q-btn
-            label="Installation guide"
+            v-if="selectedEditionID !== 'xfce'"
+            label="XFCE"
             color="accent"
-            icon="fas fa-file-download"
-            class="q-ma-sm"
-            type="a"
-            href="https://forum.xerolinux.xyz/thread-38.html"
-            target="_blank"
+            @click="selectedEditionID = 'xfce'"
+            size="18px"
           />
         </div>
       </div>
-
-      <div class="col-6 flex flex-center">
-        <div>
-          <!-- Video element -->
-          <q-video
-            :src="editions[selectedEditionID].yt"
-            class="q-mb-md"
-            id="video-monitor"
-          />
-
-          <!-- These buttons appear below the video on phone -->
-          <div
-            v-if="$q.screen.lt.md"
-            class="row q-mt-md q-gutter-sm"
-            :class="{ 'flex flex-center': $q.screen.lt.md }"
-          >
+      <div class="text-h4 q-mb-sm text-bold">
+        XeroLinux {{ editions[selectedEditionID].name }}
+      </div>
+      <div :class="{ row: $q.screen.gt.sm, column: $q.screen.lt.md }">
+        <div class="col-6 q-mt-md">
+          <p
+            class="text-justify"
+            :class="{
+              'width-control': $q.screen.gt.sm,
+            }"
+            v-html="editions[selectedEditionID].desc"
+          ></p>
+          <!-- These buttons appear under the description on desktop -->
+          <div v-if="$q.screen.gt.sm" class="row q-mt-md q-gutter-sm">
             <q-btn
               label="Release notes"
               color="accent"
@@ -125,9 +82,53 @@
             />
           </div>
         </div>
+        <div class="col-6 flex flex-center">
+          <div>
+            <!-- Video element -->
+            <q-video
+              :src="editions[selectedEditionID].yt"
+              class="q-mb-md"
+              id="video-monitor"
+            />
+            <!-- These buttons appear below the video on phone -->
+            <div
+              v-if="$q.screen.lt.md"
+              class="row q-mt-md q-gutter-sm"
+              :class="{ 'flex flex-center': $q.screen.lt.md }"
+            >
+              <q-btn
+                label="Release notes"
+                color="accent"
+                class="q-ma-sm"
+                icon="note"
+                type="a"
+                :href="editions[selectedEditionID].releaseNotes"
+                target="_blank"
+              />
+              <q-btn
+                label="Download now"
+                icon="download"
+                color="accent"
+                class="q-ma-sm"
+                type="a"
+                :href="editions[selectedEditionID].downloadLink"
+                target="_blank"
+              />
+              <q-btn
+                label="Installation guide"
+                color="accent"
+                icon="fas fa-file-download"
+                class="q-ma-sm"
+                type="a"
+                href="https://forum.xerolinux.xyz/thread-38.html"
+                target="_blank"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -182,7 +183,7 @@ export default defineComponent({
 }
 
 .width-control {
-  max-width: 650px;
+  max-width: 640px;
 }
 
 .q-btn {
